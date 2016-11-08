a29fab408eb35a40471e1d429ef5b0acbfe94258
Luke <tanprasitp@hotmail.co.uk>
2016-11-08 15:37:55 +0000

    Updated app.js

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
 var express = require('express');
 var app = express();
+var path = require('path');
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
